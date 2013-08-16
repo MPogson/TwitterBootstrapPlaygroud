86ea9d7cbc02ab30dcf5a07cf3aa510a5a13646c
MPogson <MPogson@paraport.com>
2013-08-16 16:08:37 -0700

    Re-worked the main page and added links to some of the stuff

---
+++ TwitterBootStrapPlayground/Controllers/CustomOrderController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TwitterBootStrapPlayground.Controllers
+{
+    public class CustomOrderController : Controller
+    {
+        //
+        // GET: /CustomOrder/
+
+        public ActionResult CustomOrderMain()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ TwitterBootStrapPlayground/Controllers/CustomerReviewController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TwitterBootStrapPlayground.Controllers
+{
+    public class CustomerReviewController : Controller
+    {
+        //
+        // GET: /CustomerReview/
+
+        public ActionResult CustomerReviewMain()
+        {
+            return View();
+        }
+
+    }
+}
