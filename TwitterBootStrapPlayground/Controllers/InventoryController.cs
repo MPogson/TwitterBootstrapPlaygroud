cdbabb7a4d159181213a9b8618370a5fe99bf613
MPogson <MPogson@paraport.com>
2013-08-19 13:31:49 -0700

    Trying to get the inventory page to work. no luck yet

--- TwitterBootStrapPlayground/Controllers/InventoryController.cs
+++ TwitterBootStrapPlayground/Controllers/InventoryController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FizzWare.NBuilder;
+using TwitterBootStrapPlayground.Models;
 
 namespace TwitterBootStrapPlayground.Controllers
 {
@@ -14,6 +16,16 @@ namespace TwitterBootStrapPlayground.Controllers
         public ActionResult InventoryMain()
         {
             return View();
+        }
+
+        public JsonResult GetInventory()
+        {
+            var braclets = Builder<Braclet>
+                .CreateListOfSize(50)
+                .Build();
+
+            var jsonResult = Json(new Braclet {Discontinued = false, ProductID = 100, ProductName = "Testing", UnitPrice = 100, UnitsInStock = 200}, JsonRequestBehavior.AllowGet);
+            return jsonResult;
         }
 
     }
